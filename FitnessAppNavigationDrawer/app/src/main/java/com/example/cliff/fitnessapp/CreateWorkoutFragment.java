d1002b4e434cec7cb3410053850614ba6a7d7ae8
Clifford Wilson <cswilso1@uno.edu>
2019-02-09 09:32:19 -0600

    setup database and exercise table

--- FitnessAppNavigationDrawer/app/src/main/java/com/example/cliff/fitnessapp/CreateWorkoutFragment.java
+++ FitnessAppNavigationDrawer/app/src/main/java/com/example/cliff/fitnessapp/CreateWorkoutFragment.java
@@ -1,29 +1,114 @@
 package com.example.cliff.fitnessapp;
 
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CreateWorkoutFragment extends Fragment {
+public class CreateWorkoutFragment extends Fragment implements View.OnClickListener {
 
 
-    public CreateWorkoutFragment() {
-        // Required empty public constructor
-    }
+    //TODO remove this
+    //just placeholder junk for testing database
+    private ArrayList<String> exerciseNames = new ArrayList<String>();
+    private String exerciseNamesString;
+
+    //Required empty public constructor
+    public CreateWorkoutFragment() {}
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_workout, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_create_workout, container, false);
+
+        Button b = (Button) v.findViewById(R.id.add_workout_button);
+        b.setOnClickListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onStart()
+    {
+        Spinner spinner = (Spinner) getView().findViewById(R.id.name_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.workouts_array, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        super.onStart();
+    }
+
+    public void onClick(View v)
+    {
+        System.out.println("BUTTON PRESSED");
+
+        int id = v.getId();
+
+        if (id == R.id.add_workout_button)
+        {
+            SQLiteOpenHelper helper = new FitnessAppHelper(getActivity());
+
+            try
+            {
+                SQLiteDatabase db = helper.getWritableDatabase();
+
+                String workoutName = (String)(((Spinner)getView().findViewById(R.id.name_spinner)).getSelectedItem().toString());
+                int reps = Integer.valueOf((((EditText)getView().findViewById(R.id.rep_edit_text)).getText().toString()));
+                int repCount = Integer.valueOf((((EditText)getView().findViewById(R.id.rep_count_edit_text)).getText().toString()));
+                int weight = Integer.valueOf((((EditText)getView().findViewById(R.id.weight_edit_text)).getText().toString()));
+
+                String test = String.format("Name %s reps %d repcount %d, weight %d", workoutName, reps, repCount, weight);
+                System.out.println(test);
+
+                ContentValues exerciseValues = new ContentValues();
+                exerciseValues.put("NAME", workoutName);
+                exerciseValues.put("REPS", reps);
+                exerciseValues.put("NUMBEROFREPS", repCount);
+                exerciseValues.put("WEIGHT", weight);
+
+                db.insert("EXERCISE", null, exerciseValues);
+
+                String[] columns = {"WEIGHT"};
+                Cursor cursor = db.query("EXERCISE", columns, null, null, null, null, null);
+                cursor.moveToFirst();
+
+                String weightValues = "";
+                do
+                {
+                    weightValues += " " + String.valueOf(cursor.getInt(0));
+                }
+                while (cursor.moveToNext());
+
+                TextView databaseContents = getView().findViewById(R.id.exercise_database_contents);
+                databaseContents.setText(weightValues);
+            }
+            catch (SQLiteException e)
+            {
+                Toast toast = Toast.makeText(getActivity(), "Database Unavailabe", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
     }
 
 }
---
+++ FitnessAppNavigationDrawer/app/src/main/java/com/example/cliff/fitnessapp/FitnessAppHelper.java
@@ -0,0 +1,72 @@
+package com.example.cliff.fitnessapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FitnessAppHelper extends SQLiteOpenHelper
+{
+    private static final String DB_NAME = "Fitness_App";
+    private static final int DB_VERSION = 1;
+
+    FitnessAppHelper(Context context)
+    {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db)
+    {
+        updateMyDatabase(db, 0, DB_VERSION);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        //If the old version is less than 1 then that means the database has not been created
+        //so the tables must be made for the first time
+        if (oldVersion < 1)
+        {
+            System.out.println("DATABASE IS BEING CREATED FOR THE FIRST TIME!");
+            db.execSQL("CREATE TABLE EXERCISE (" +
+                    "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "NAME TEXT," +
+                    "REPS INTEGER," +
+                    "NUMBEROFREPS INTEGER," +
+                    "WEIGHT INTEGER," +
+                    "USESWEIGHT INTEGER);");
+
+            db.execSQL("CREATE TABLE WORKOUT (" +
+                    "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "NAME TEXT," +
+                    "EXERCISE1 INTEGER," +
+                    "EXERCISE2 INTEGER," +
+                    "EXERCISE3 INTEGER);");
+        }
+        if (oldVersion < 2)
+        {
+            //For future use
+        }
+
+    }
+
+    //helper method to add rows to the exercise table
+    private static void insertExercise(SQLiteDatabase db, String name, int reps, int numberOfReps, int weight, int usesWeight)
+    {
+        ContentValues exerciseValues = new ContentValues();
+        exerciseValues.put("NAME", name);
+        exerciseValues.put("REPS", reps);
+        exerciseValues.put("NUMBEROFREPS", numberOfReps);
+        exerciseValues.put("WEIGHT", weight);
+        exerciseValues.put("USESWEIGHT", usesWeight);
+    }
+
+    //helper method to add rows to the workout table
+    private static void insertWorkout()
+    {
+    }
+}
