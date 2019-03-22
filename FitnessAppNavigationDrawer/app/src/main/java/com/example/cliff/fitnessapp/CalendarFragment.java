5a81774219b69d5f239fd1191d50c83a63d923b5
Benjamin Rongey <benjamin.rongey@gmail.com>
2019-03-22 12:44:18 -0500

    got basic calendar view up and running will continue to tweak

--- FitnessAppNavigationDrawer/app/src/main/java/com/example/cliff/fitnessapp/CalendarFragment.java
+++ FitnessAppNavigationDrawer/app/src/main/java/com/example/cliff/fitnessapp/CalendarFragment.java
@@ -1,9 +1,23 @@
 package com.example.cliff.fitnessapp;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CalendarFragment extends Fragment {
 
+    public CalendarFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        return v;
+    }
 
 
 }
